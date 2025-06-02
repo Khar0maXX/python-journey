55d92842feaf9b1d280e881862a0355258d1dabc
samuel woiye <neo@matrix>
2025-06-02 08:18:37 +0300

    Mission 1.1: Data Capture Module completed

---
+++ mission_1_1.py
@@ -0,0 +1,7 @@
+name = input("Engineer name: ")
+role = input("Assigned role: ")
+planet = input("Planet of origin: ")
+language = input("Favourite programming language: ")
+
+print("\n Mision Report:")
+print(f"Engineer {name} ({role}) from {planet} prefers {language}.\n Mission data captured successfully.")
